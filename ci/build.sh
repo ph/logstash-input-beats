134532e75e8fa2a2b4754eb172a90b12c516c58a
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-05-19 15:11:26 -0400

    Use external file to separate the build process

--- ci/build.sh
+++ ci/build.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
+########################################################
+#
+# AUTOMATICALLY GENERATED! DO NOT EDIT
+#
+# version: 1
+########################################################
 set -e
 
 echo "Starting build process in: `pwd`"
+./ci/setup.sh
 
-if [ "$LOGSTASH_BRANCH" ]; then
-    BASE_DIR=`pwd`
-    echo "Checking out branch: $LOGSTASH_BRANCH"
-    git clone -b $LOGSTASH_BRANCH https://github.com/elastic/logstash.git ../../logstash --depth 1
-    printf "Checked out Logstash revision: %s\n" "$(git -C ../../logstash rev-parse HEAD)"
-    cd ../../logstash
-    echo "Building plugins with Logstash version:"
-    cat versions.yml
-    echo "---"
-    # We need to build the jars for that specific version
-    echo "Running gradle assemble in: `pwd`"
-    ./gradlew assemble
-    cd $BASE_DIR
-    export LOGSTASH_SOURCE=1
+if [[ -f "ci/run.sh" ]];
+    echo "Running custom build script in: `pwd`"
+    ./ci/run.sh
+else
+    echo "Running default build scripts in: `pwd`"
+    bundle install
+    bundle exec rspec spec
+    bundle exec rspec spec --tag integration
 fi
-
-echo "Running install in: `pwd`"
-bundle install
-bundle exec rake vendor
-./gradlew test
-bundle exec rspec spec
-bundle exec rake test:integration:setup
-bundle exec rspec spec --tag integration
